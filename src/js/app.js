b8676240be1ce4ba298a2450c7e2f3c4718651f8
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-02-12 11:03:14 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -197,3 +197,21 @@ document.querySelectorAll('.products__item').forEach(function ( el ) {
     })
   })
 })
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
